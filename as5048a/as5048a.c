c3350c8ae0e512735cd26fb3e3bb36c9376007e8
paltus23 <paltusoff@gmail.com>
2026-02-28 19:53:24 +0400

    add driver as5048a

---
+++ as5048a/as5048a.c
@@ -0,0 +1,255 @@
+/**
+ * @file as5048a.c
+ * @author paltusoff@gmail.com
+ * @brief  A library for AMS AS5048A rotary position sensor/magnetic encoder.
+ *        Just a remake from AS5047P library.
+ * @copyright MIT License, Copyright (c) 2026
+ *
+ */
+
+#include "as5048a.h"
+
+#define BIT_MODITY(src, i, val) ((src) ^= (-(val) ^ (src)) & (1UL << (i)))
+#define BIT_READ(src, i) (((src) >> (i) & 1U))
+#define BIT_TOGGLE(src, i) ((src) ^= 1UL << (i))
+
+/* Volatile register address. */
+#define AS5048A_NOP ((uint16_t)0x0000)
+#define AS5048A_ERRFL ((uint16_t)0x0001)
+#define AS5048A_PROG ((uint16_t)0x0003)
+#define AS5048A_DIAAGC ((uint16_t)0x3FFD)
+#define AS5048A_MAG ((uint16_t)0x3FFE)
+#define AS5048A_ANGLE ((uint16_t)0x3FFF)
+
+/* Non-Volatile register address. */
+#define AS5048A_ZPOSM ((uint16_t)0x0016)
+#define AS5048A_ZPOSL ((uint16_t)0x0017)
+
+#define OP_WRITE ((uint8_t)0)
+#define OP_READ ((uint8_t)1)
+
+static void send_command(const as5048a_handle_t *as5048a_handle, uint16_t address, uint8_t op_read_write);
+static void send_data(const as5048a_handle_t *as5048a_handle, uint16_t address, uint16_t data);
+static uint16_t read_data(const as5048a_handle_t *as5048a_handle, uint16_t address);
+
+void spi_transmit(const as5048a_handle_t *as5048a_handle, uint16_t data);
+uint16_t spi_receive(const as5048a_handle_t *as5048a_handle);
+
+void as5048a_nop(const as5048a_handle_t *as5048a_handle);
+void delay(volatile uint16_t t);
+uint8_t is_even_parity(uint16_t data);
+
+/**
+ * @brief Make a AS5048A handle.
+ *
+ * @param[in] spi_send_func
+ * @param[in] spi_read_func
+ * @param[in] spi_select_func
+ * @param[in] spi_deselect_func
+ * @param[in] delay_func The function that delay 350ns for t_CSn.
+ * @param[out] as5048a_handle AS5048A handle.
+ * @return Status code.
+ *         0: Success.
+ */
+int8_t as5048a_make_handle(as5048a_spi_send_t spi_send_func,
+                           as5048a_spi_read_t spi_read_func,
+                           as5048a_spi_deselect_t spi_select_func,
+                           as5048a_spi_deselect_t spi_deselect_func,
+                           as5048a_delay_t delay_func,
+                           as5048a_handle_t *as5048a_handle)
+{
+  as5048a_handle->spi_send = spi_send_func;
+  as5048a_handle->spi_read = spi_read_func;
+  as5048a_handle->spi_select = spi_select_func;
+  as5048a_handle->spi_deselect = spi_deselect_func;
+  as5048a_handle->delay = delay_func;
+  return 0; /* Success. */
+}
+
+/**
+ * @brief Reading error flags.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @return Error flags. 0 for no error occurred.
+ */
+uint16_t as5048a_get_error_status(const as5048a_handle_t *as5048a_handle)
+{
+  return read_data(as5048a_handle, AS5048A_ERRFL);
+}
+
+/**
+ * @brief Read current position.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param position Current position raw value.
+ * @return Status code.
+ *         0: Success.
+ *         -1: Error occurred.
+ */
+int8_t as5048a_get_position(const as5048a_handle_t *as5048a_handle,
+                            uint16_t *position)
+{
+  uint16_t data = read_data(as5048a_handle, AS5048A_ANGLE);
+  if (BIT_READ(data, 14) == 0)
+  {
+    *position = data & 0x3FFF;
+    return 0; /* No error occurred. */
+  }
+  return -1; /* Error occurred. */
+}
+
+/**
+ * @brief Read current angle in degree.
+ *
+ * @param as5048a_handle AS5048A handle
+ * @param angle_degree Current angle in degree.
+ * @return Status code.
+ *         0: Success.
+ *         -1: Error occurred.
+ */
+int8_t as5048a_get_angle(const as5048a_handle_t *as5048a_handle, float *angle_degree)
+{
+  uint16_t raw_position;
+  int8_t error = as5048a_get_position(as5048a_handle, &raw_position);
+  if (error == 0)
+  {
+    /* Angle in degree = value * ( 360 / 2^14). */
+    *angle_degree = raw_position * (360.0 / 0x4000);
+  }
+
+  return error;
+}
+
+/**
+ * @brief Set specify position as zero.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param position Position raw value.
+ */
+void as5048a_set_zero(const as5048a_handle_t *as5048a_handle, uint16_t position)
+{
+  /* 8 most significant bits of the zero position. */
+  send_data(as5048a_handle, AS5048A_ZPOSM, ((position >> 6) & 0x00FF));
+
+  /* 6 least significant bits of the zero position. */
+  send_data(as5048a_handle, AS5048A_ZPOSL, (position & 0x003F));
+
+  as5048a_nop(as5048a_handle);
+}
+
+/**
+ * @brief No operation instruction.
+ *
+ * @param as5048a_handle AS5048A handle.
+ */
+inline void as5048a_nop(const as5048a_handle_t *as5048a_handle)
+{
+  /* Reading the NOP register is equivalent to a nop (no operation) instruction. */
+  send_command(as5048a_handle, AS5048A_NOP, OP_READ);
+}
+
+/**
+ * @brief Sending read or write command to AS5048A.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param address Register address.
+ * @param op_read_write Read of write opration.
+ */
+static void send_command(const as5048a_handle_t *as5048a_handle, uint16_t address, uint8_t op_read_write)
+{
+  uint16_t frame = address & 0x3FFF;
+
+  /* R/W: 0 for write, 1 for read. */
+  BIT_MODITY(frame, 14, op_read_write);
+
+  /* Parity bit(even) calculated on the lower 15 bits. */
+  if (!is_even_parity(frame))
+  {
+    BIT_TOGGLE(frame, 15);
+  }
+
+  spi_transmit(as5048a_handle, frame);
+}
+
+/**
+ * @brief Sending data to register.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param address Register address.
+ * @param data Data.
+ */
+static void send_data(const as5048a_handle_t *as5048a_handle, uint16_t address, uint16_t data)
+{
+  uint16_t frame = data & 0x3FFF;
+
+  /* Data frame bit 14 always low(0). */
+  BIT_MODITY(frame, 14, 0);
+
+  /* Parity bit(even) calculated on the lower 15 bits. */
+  if (!is_even_parity(frame))
+  {
+    BIT_TOGGLE(frame, 15);
+  }
+
+  send_command(as5048a_handle, address, OP_WRITE);
+  spi_transmit(as5048a_handle, frame);
+}
+
+/**
+ * @brief Reading data from register.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param address Register address.
+ * @return Data.
+ */
+static uint16_t read_data(const as5048a_handle_t *as5048a_handle, uint16_t address)
+{
+  send_command(as5048a_handle, address, OP_READ);
+  return spi_receive(as5048a_handle);
+}
+
+/**
+ * @brief Start SPI transmit.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @param data Data.
+ */
+inline void spi_transmit(const as5048a_handle_t *as5048a_handle, uint16_t data)
+{
+  as5048a_handle->delay();
+
+  as5048a_handle->spi_select();
+  as5048a_handle->spi_send(data);
+  as5048a_handle->spi_deselect();
+}
+
+/**
+ * @brief Start SPI receive.
+ *
+ * @param as5048a_handle AS5048A handle.
+ * @return Received data.
+ */
+inline uint16_t spi_receive(const as5048a_handle_t *as5048a_handle)
+{
+  as5048a_handle->delay();
+
+  as5048a_handle->spi_select();
+  uint16_t data = as5048a_handle->spi_read();
+  as5048a_handle->spi_deselect();
+
+  return data;
+}
+
+/**
+ * @brief Check data even parity.
+ */
+uint8_t is_even_parity(uint16_t data)
+{
+  uint8_t shift = 1;
+  while (shift < (sizeof(data) * 8))
+  {
+    data ^= (data >> shift);
+    shift <<= 1;
+  }
+  return !(data & 0x1);
+}
---
+++ as5048a/as5048a.h
@@ -0,0 +1,67 @@
+/**
+ * @file as5048a.h
+ * @author paltusoff@gmail.com
+ * @brief  A library for AMS AS5048A rotary position sensor/magnetic encoder.
+ *        Just a remake from AS5047P library.
+ * @copyright MIT License, Copyright (c) 2026
+ * @remark AS5048A SPI Interface:
+ *         - Mode=1(CPOL=0, CPHA=1).
+ *             - CPOL=0 --> Clock is low when idle.
+ *             - CPHA=1 --> Data is sampled on the second edge(falling edge).
+ *         - CSn(chip select) active low.
+ *         - Data size=16-bit.
+ *         - Bit order is MSB first.
+ *         - Max clock rates up to 10 MHz.
+ *         - Only supports slave operation mode.
+ *
+ */
+
+#ifndef AS5048A_H
+#define AS5048A_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+
+  typedef void (*as5048a_spi_send_t)(uint16_t data);
+  typedef uint16_t (*as5048a_spi_read_t)(void);
+  typedef void (*as5048a_spi_select_t)(void);
+  typedef void (*as5048a_spi_deselect_t)(void);
+
+  /**
+   * @brief t_CSn: High time of CSn between two transmissions, Min: 350 ns.
+   */
+  typedef void (*as5048a_delay_t)(void);
+
+  typedef struct
+  {
+    as5048a_spi_send_t spi_send;
+    as5048a_spi_read_t spi_read;
+    as5048a_spi_select_t spi_select;
+    as5048a_spi_deselect_t spi_deselect;
+    as5048a_delay_t delay;
+  } as5048a_handle_t;
+
+  int8_t as5048a_make_handle(as5048a_spi_send_t spi_send_func,
+                             as5048a_spi_read_t spi_read_func,
+                             as5048a_spi_deselect_t spi_select_func,
+                             as5048a_spi_deselect_t spi_deselect_func,
+                             as5048a_delay_t delay_func,
+                             as5048a_handle_t *as5048a_handle);
+
+  void as5048a_set_zero(const as5048a_handle_t *as5048a_handle, uint16_t position);
+
+  int8_t as5048a_get_position(const as5048a_handle_t *as5048a_handle, uint16_t *position);
+
+  int8_t as5048a_get_angle(const as5048a_handle_t *as5048a_handle, float *angle_degree);
+
+  uint16_t as5048a_get_error_status(const as5048a_handle_t *as5048a_handle);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AS5048A_H */
